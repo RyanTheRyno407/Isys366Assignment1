09d57023d42c41c2a01f320a94871a838aa75816
Ryan hambrick <ryanhambrick14@gmail.com>
2026-02-27 18:54:30 -0500

    Final

--- Movie practice/RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ Movie practice/RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -1,6 +1,26 @@
-﻿namespace RazorPagesMovie.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [Range(1, 10)]
+        public int Rank {  get; set; }
     }
 }
